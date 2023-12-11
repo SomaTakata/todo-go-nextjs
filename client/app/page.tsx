1168868085b8a0540752eaf988cfb4816c93e32d
Soma Takata <hayatetaka123@gmail.com>
2023-12-11 16:59:33 +0900

    feat: 🎸 backend 実装

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,9 +1,19 @@
+"use client";
 import SideBar from "@/components/SideBar";
 import { Badge } from "@/components/ui/badge";
 import { Card } from "@/components/ui/card";
 import { PlusCircle } from "lucide-react";
 import { Trash2 } from "lucide-react";
+import useSWR from "swr";
+
+export const ENDPOINT = "http://localhost:4000";
+
+const fetcher = (url: string) =>
+  fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
+
 export default function Home() {
+  const { data, mutate } = useSWR("api/todos", fetcher);
+
   return (
     <main className="flex h-screen w-fll items-center justify-center ">
       <div className="flex h-screen w-full items-center justify-center gap-4">
@@ -12,7 +22,7 @@ export default function Home() {
           <div className=" w-[90%] flex flex-col mt-5">
             <div className="flex m-2 justify-between">
               <div className="font-bold text-lg border-b-2 border-teal-400">
-                All Tasks
+                All Tasks/{JSON.stringify(data)}
               </div>
               <PlusCircle />
             </div>
@@ -38,6 +48,10 @@ export default function Home() {
                     <Trash2 className="w-5 h-5 mr-2" />
                   </div>
                 </div>
+              </Card>
+
+              <Card className="w-full h-[150px]   relative">
+                <button className="w-full h-full">add</button>
               </Card>
             </div>
           </div>
