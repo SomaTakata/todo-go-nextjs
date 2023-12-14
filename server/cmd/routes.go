e00016bed673deec84d0b7859b238e1fae62d130
SomaTakata <somatakata@gmail.com>
2023-12-14 12:21:11 +0900

    feat: 🎸 完了、未完了の切り替え機能実装

--- server/cmd/routes.go
+++ server/cmd/routes.go
@@ -9,5 +9,8 @@ func setupRoutes(app *fiber.App) {
 	app.Get("/api/todos", handlers.ListTodos)
 
 	app.Post("/api/todos", handlers.CreateTodo)
+
+	app.Patch("/api/todos/:id/done", handlers.UpdateDone)
+
 	app.Delete("/api/todos/:id", handlers.DeleteTodo)
 }
