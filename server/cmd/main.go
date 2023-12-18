8239c88de05a56de03babda89b0e358343a72beb
Soma Takata <hayatetaka123@gmail.com>
2023-12-19 00:09:36 +0900

    feat: 🎸 コメントの追加

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -9,15 +9,22 @@ import (
 )
 
 func main() {
+	// データベースへの接続を初期化
 	database.ConnectDb()
+
+	// 新しいFiberアプリケーションのインスタンスを作成
 	app := fiber.New()
+
+	// CORSミドルウェアを設定
+	// これにより、localhost:3000 からのリクエストを受け入れることができる
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: "http://localhost:3000",
 		AllowHeaders: "Origin, Content-Type, Accept",
 	}))
 
+	// ルーティングの設定（setupRoutes関数はおそらく他の場所で定義されている）
 	setupRoutes(app)
 
+	// サーバーをポート8080で起動し、エラーが発生した場合はログに記録
 	log.Fatal(app.Listen(":8080"))
-
 }
